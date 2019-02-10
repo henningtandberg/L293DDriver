653c36e63cf21576eb27b4facc32fa6ca2232974
Henning P. Tandberg <henningtandberg@gmail.com>
2019-02-10 22:12:10 +0100

    Fix code

--- L293DDriver.cpp
+++ L293DDriver.cpp
@@ -1,52 +1,87 @@
 #include "L293DDriver.h"
+#include "Arduino.h"
 
-/*
-*   Public
-*/
-
-L293DDriver::L293DDriver(int ep1, int ep2) {
-    _spd1 = _spd2 = 0;
-    _ep1 = ep1;
-    _ep2 = ep2;
+/** L293DDRiver() - Contructor 1.
+ *  @mot1_ctrl1:
+ *  @mot1_ctrl2:
+ *  @mot2_ctrl1:
+ *  @mot2_ctrl2:
+ *  @mot1_ep:
+ *  @mot2_ep:
+ *
+ */
+L293DDriver::L293DDriver(int mot1_ctrl1, int mot1_ctrl2, int mot2_ctrl1, int mot2_ctrl2, int mot1_ep, int mot2_ep) {
+   motors[MOT_1] = {
+        .ctrl1 = mot1_ctrl1,
+        .ctrl2 = mot1_ctrl2,
+        .ep = mot1_ep,
+        .spd = 0,
+        .dir = DIR_FWD
+    };    
+    motors[MOT_2] = {
+        .ctrl1 = mot2_ctrl1,
+        .ctrl2 = mot2_ctrl2,
+        .ep = mot2_ep,
+        .spd = 0,
+        .dir = DIR_FWD
+    };
 }
 
-int L293DDriver::init(void) {
-    if(_ep1 == _ep2)
-        return -1;
-
-    //Add later?
-    /*if(!isPWM(_ep1) || !isPWM(_ep2))
-        return -1;*/
-
-    pinMode(_ep1, OUTPUT);
-    pinMode(_ep2, OUTPUT);
-
-    pinMode(I1, OUTPUT);
-    pinMode(I2, OUTPUT);
-    pinMode(I3, OUTPUT);
-    pinMode(I4, OUTPUT);
-
-    return 0;
+/** L293DDriver() - Contructor 2.
+ *  @mot1_ep:
+ *  @mot2_ep:
+ *
+ */
+L293DDriver::L293DDriver(int mot1_ep, int mot2_ep) {
+    this->L293Driver(
+        MOT1_CTRL1,
+        MOT1_CTRL2,
+        MOT2_CTRL1,
+        MOT2_CTRL2,
+        mot1_ep,
+        mot2_ep
+    );
 }
 
-void L293DDriver::speed(int motor, uint8_t speed) {
+/** init() - Initializes the driver.
+ *
+ */
+void L293DDriver::init(void) {
+    if(motors[MOT_1].ep == motors[MOT_2].ep)
+        while(1);
 
-    switch (motor) {
-        case M1:
-            _spd1 = speed;
-            break;
-        case M2:
-            _spd2 = speed;
-            break;
-        case MALL:
-            _spd1 = _spd2 = speed;
-            break;
-        default:
-            break;
+    // TODO: isPWM
+    
+    for (int i = 0; i < MOT_ALL; i++) {
+        pinMode(motors[i].ep, OUTPUT);
+        pinMode(motors[i].ctrl1, OUTPUT);
+        pinMode(motors[i].ctrl2, OUTPUT);
     }
+}
+
+/** setSpeed() - Sets the speed of the specified motor.
+ *  @motor:
+ *  @speed:
+ *
+ */
+void L293DDriver::setSpeed(int motor, uint8_t speed) {
+
+    if (motor > MOT_ALL || motor < MOT_1)
+        return;
+
+    if (motor == MOT_ALL)
+        for (int i = 0; i < MOT_ALL; i++)
+            motors[i].spd = speed;
+    else
+        motors[motor].spd = speed;
 
 }
 
+/** setDirection() - Sets the direction of the specified motor.
+ *  @motor:
+ *  @direction:
+ *
+ */
 void L293DDriver::direction(int motor, int dir) {
     /*if(dir) {
 
@@ -55,50 +90,36 @@ void L293DDriver::direction(int motor, int dir) {
     }*/
 }
 
+/** drive() - Activates the specified motor.
+ *  @motor:
+ *
+ */
 void L293DDriver::drive(int motor) {
 
-    switch (motor) {
-        case M1:
-            analogWrite(_ep1, _spd1);
-            break;
+    if (motor > MOT_ALL || motor < MOT_1)
+        return;
 
-        case M2:
-            analogWrite(_ep2, _spd2);
-            break;
-
-        case MALL:
-            analogWrite(_ep1, _spd1);
-            analogWrite(_ep2, _spd2);
-            break;
-
-        default:
-            break;
-    }
+    if (motor == MOT_ALL)
+        for (int i = 0; i < MOT_ALL; i++)
+            analogWrite(motors[i].ep, motors[i].spd);
+    else
+        analogWrite(motors[motor].ep, motors[i].spd);
 
 }
 
+/** stop() - Deactivates the specified motor.
+ *  @motor:
+ *
+ */
 void L293DDriver::stop(int motor) {
 
-    switch (motor) {
-        case M1:
-            analogWrite(_ep1, _spd1);
-            break;
+    if (motor > MOT_ALL || motor < MOT_1)
+        return;
 
-        case M2:
-            digitalWrite(_ep2, LOW);
-            break;
-
-        case MALL:
-            digitalWrite(_ep1, LOW);
-            digitalWrite(_ep2, LOW);
-            break;
-
-        default:
-            break;
-    }
+    if (motor == MOT_ALL)
+        for (int i = 0; i < MOT_ALL; i++)
+            analogWrite(motors[i].ep, LOW);
+    else
+        analogWrite(motors[motor].ep, LOW);
 
 }
-
-/*
-*   Private
-*/
--- L293DDriver.h
+++ L293DDriver.h
@@ -3,53 +3,73 @@
 *   File:   L293DDriver.h
 *
 *   Pinout:
-*   L293D   -   Arduino |
-*   1       -   a PWM pin | 1,2EN
-*   2       -   8       | A1
-*   3       -   One terminal on motor 1
-*   4       -   UNUSED
-*   5       -   UNUSED
-*   6       -   Other terminal on motor 1
-*   7       -   7       | A2
-*   8       -   +ve of battery
-*   9       -   a PWM pin | 3,4EN
-*   10      -   4       | A3
-*   11      -   One terminal on motor 2
-*   12      -   UNUSED
-*   13      -   GND (battery and arduino)
-*   14      -   Other terminal on motor 2
-*   15      -   3       | A4
-*   16      -   VCC (5V)
+*   ---------------------------------------------
+*   |   L293D   |   Arduino                     |
+*   ---------------------------------------------
+*   |   1       |   A PWM pin | 1,2EN           |
+*   |   2       |   Control pin 1 motor 1       |
+*   |   3       |   One terminal on motor 1     |
+*   |   4       |   UNUSED                      |
+*   |   5       |   UNUSED                      |
+*   |   6       |   Other terminal on motor 1   |
+*   |   7       |   Control pin 2 motor 1       |
+*   |   8       |   +ve of battery              |
+*   |   9       |   A PWM pin | 3,4EN           |
+*   |   10      |   Control pin 1 motor 2       |
+*   |   11      |   One terminal on motor 2     |
+*   |   12      |   UNUSED                      |
+*   |   13      |   GND (battery and arduino)   |   
+*   |   14      |   Other terminal on motor 2   |
+*   |   15      |   Control pin 2 motor 2       |
+*   |   16      |   VCC (5V)                    |
+*   ---------------------------------------------
 */
+
 #ifndef _L293DDRIVER_H
 #define _L293DDRIVER_H
 
-#include "Arduino.h"
+/** Default pins if not specified */
+#define MOT1_CTRL1  8
+#define MOT1_CTRL2  7
+#define MOT2_CTRL1  4
+#define MOT2_CTRL2  3
 
-#define I1  8   //Control pin 1 motor 1
-#define I2  7   //Control pin 2 motor 1
-#define I3  4   //Control pin 1 motor 2
-#define I4  3   //Control pin 2 motor 2
+/** Motor directrions */
+#define DIR_FWD     1
+#define DIR_BWD     0
 
-#define M1     0
-#define M2     1
-#define MALL   2
-#define FWD    1
-#define BWD    0
+/** Indecies of motors */
+#define MOT_1       0
+#define MOT_2       1
+#define MOT_ALL     2
 
-class L293DDriver
-{
+/* struct motor - Values assosiated with a motor
+ *  @ctrl1: Control pin 1
+ *  @ctrl2: Control pin 2
+ *  @ep:    PWM pin
+ *  @spd:   Current speed of motor
+ *  @dir:   Current directon of motor
+ */
+typedef struct motor {
+    uint8_t ctrl1;
+    uint8_t ctrl2;
+    uint8_t ep;
+    uint8_t spd;
+    uint8_t dir;
+} Motor;
+
+class L293DDriver {
     public:
-        L293DDriver(int ep1, int ep2);
+        L293DDriver(int mot1_ctrl1, int mot1_ctrl2, int mot2_ctrl1, int mot2_ctrl2, int mot1_ep, int mot2_ep);
+        L293DDriver(int mot1Ctrl1, int mot1int ep1, int ep2);
         int init(void);
-        void speed(int motor, uint8_t speed);
-        void direction(int motor, int dir);
+        void setSpeed(int motor, uint8_t speed);
+        void setDirection(int motor, int direction);
         void drive(int motor);
         void stop(int motor);
 
     private:
-        int _ep1, _ep2;
-        uint8_t _spd1, _spd2;
+        Motor motor[MOT_ALL];
 };
 
 #endif
