bdbfbc850a3dcbcb87164dce0b4ea95e76c498c6
Henning P. Tandberg <henningtandberg@gmail.com>
2019-02-12 09:42:36 +0100

    Add comments

--- L293DDriver.cpp
+++ L293DDriver.cpp
@@ -1,3 +1,7 @@
+/** Author: Henning P. Tandberg
+ *  File:   L293DDriver.cpp
+ */
+
 #include "L293DDriver.h"
 #include "Arduino.h"
 
@@ -6,12 +10,12 @@
  */
 
 /** L293DDRiver() - Contructor 1.
- *  @m1_inPin1:
- *  @m1_inPin2:
- *  @m2_inPin1:
- *  @m2_inPin2:
- *  @m1_pwmPin:
- *  @m2_pwmPin:
+ *  @m1_inPin1: Input pin 1 for motor 1
+ *  @m1_inPin2: Input pin 2 for motor 1
+ *  @m2_inPin1: Input pin 1 for motor 2
+ *  @m2_inPin2: Input pin 2 for motor 2
+ *  @m1_pwmPin: PWM pin for motor 1
+ *  @m2_pwmPin: PWM pin for motor 2
  *
  */        
 L293DDriver::L293DDriver(uint8_t m1_inPin1, uint8_t m1_inPin2, uint8_t m2_inPin1, uint8_t m2_inPin2, uint8_t m1_pwmPin, uint8_t m2_pwmPin) {
@@ -20,8 +24,8 @@ L293DDriver::L293DDriver(uint8_t m1_inPin1, uint8_t m1_inPin2, uint8_t m2_inPin1
 }
 
 /** L293DDriver() - Contructor 2.
- *  @m1_pwm:
- *  @m2_pwm:
+ *  @m1_pwm: PWM pin for motor 1
+ *  @m2_pwm: PWM pin for motor 2
  *
  */
 L293DDriver::L293DDriver(uint8_t m1_pwmPin, uint8_t m2_pwmPin) :
@@ -36,7 +40,10 @@ void L293DDriver::begin(void) {
     }
 }
 
-
+/** getDCMotor() - Returns a motor object.
+ *  @motor: Index of motor (M1 or M2)
+ *
+ */
 L293DDCMotor * L293DDriver::getDCMotor(uint8_t motor) {
     if (motor < M1 || motor > M2) {
         return NULL;
@@ -49,7 +56,10 @@ L293DDCMotor * L293DDriver::getDCMotor(uint8_t motor) {
  *  L293DDCMotor
  */
 
-/**
+/** L293DDCMotor() - Constructor 1.
+ *  @pwmPin: PWM pin for motor speed control
+ *  @inPin1: Input pin 1
+ *  @inPin2: Input pin 2
  *
  */
 L293DDCMotor::L293DDCMotor(uint8_t pwmPin, uint8_t inPin1, uint8_t inPin2) {
@@ -60,7 +70,7 @@ L293DDCMotor::L293DDCMotor(uint8_t pwmPin, uint8_t inPin1, uint8_t inPin2) {
     this->direction = DIR_STOP;
 }
 
-/**
+/** L293DDCMotor() - Constructor 2.
  *
  */
 L293DDCMotor::L293DDCMotor(void) {
@@ -71,6 +81,9 @@ L293DDCMotor::L293DDCMotor(void) {
     this->direction = DIR_STOP;
 }
 
+/** init() - Initializes the motor (pins, speed and direction)
+ *
+ */
 void L293DDCMotor::init(void) {
     pinMode(this->pwmPin, OUTPUT);
     pinMode(this->inPin1, OUTPUT);
@@ -82,7 +95,7 @@ void L293DDCMotor::init(void) {
 }
 
 /** setSpeed() - Sets the speed of the specified motor.
- *  @speed:
+ *  @speed: New Speed of motor.
  *
  */
 void L293DDCMotor::setSpeed(uint8_t speed) {
@@ -91,8 +104,12 @@ void L293DDCMotor::setSpeed(uint8_t speed) {
 }
 
 /** setDirection() - Sets the direction of the specified motor.
- *  @direction:
+ *  @direction: New direction of motor.
  *
+ *  Direction options:
+ *      - DIR_FWD   (Clockwise)
+ *      - DIR_BWD   (Anti clockwise)
+ *      - DIR_STOP  (Stop the motor)
  */
 void L293DDCMotor::drive(uint8_t direction) {
     switch (direction) {
